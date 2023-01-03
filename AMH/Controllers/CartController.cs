e71a0b4657f09cb89fc00e9a03ec1881270761a7
DESKTOP-AHU6Q7H\DELL <dishazalavadiya6@gmail.com>
2023-01-03 21:44:07 +0530

    c

--- AMH/Controllers/CartController.cs
+++ AMH/Controllers/CartController.cs
@@ -9,25 +9,25 @@ namespace AMH.Controllers
 
     public class CartController : Controller
     {
-        public readonly AbstractCartServices abstractCartServices;
+    //    public readonly AbstractCartServices abstractCartServices;
 
-        public CartController(AbstractCartServices abstractCartServices)
-        {
-            this.abstractCartServices = abstractCartServices;
-        }
-        // GET: Home
+    //    public CartController(AbstractCartServices abstractCartServices)
+    //    {
+    //        this.abstractCartServices = abstractCartServices;
+    //    }
+    //    // GET: Home
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public JsonResult Cart_Upsert(int Cart_Id = 0)
-        {
-            Cart model = new Cart();
-            model.Id = Cart_Id;
-            var result = abstractCartServices.Cart_Upsert(model);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
+    //    public ActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //    [HttpPost]
+    //    public JsonResult Cart_Upsert(int Cart_Id = 0)
+    //    {
+    //        Cart model = new Cart();
+    //        model.Id = Cart_Id;
+    //        var result = abstractCartServices.Cart_Upsert(model);
+    //        return Json(result, JsonRequestBehavior.AllowGet);
+    //    }
     }
 }
